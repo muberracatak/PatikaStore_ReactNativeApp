dfec6582f63eaed97e89465f00af2bf9e06514c9
Müberra <55498680+muberracatak@users.noreply.github.com>
2022-02-07 14:21:32 +0300

    Add files via upload

---
+++ SearchBar/SearchBar.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create(
+    {
+        container:{
+            backgroundColor:'#eceff1',
+            padding:8,
+            margin:10,
+            marginTop:15,
+            borderRadius:5,
+        },
+    }
+)
+
