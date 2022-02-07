67eba29c1d4d3372993e0691bb210245dfce01a7
Müberra <55498680+muberracatak@users.noreply.github.com>
2022-02-07 14:20:32 +0300

    Update App_m.js

--- src/App_m.js
+++ src/App_m.js
@@ -2,26 +2,39 @@ import React, { useState }  from 'react';
 import { SafeAreaView,Image,StyleSheet,View,Text,FlatList,ScrollView, Dimensions } from "react-native";
 import news_data_m from './news_data_m.json'; 
 import NewsCard2 from './components/NewsCard/NewsCard2';
-import SearchBar from './components/NewsCard/SearchBar';
+import SearchBar from "./components/SearchBar";
 import Screen from './components/NewsCard/Screen';
 
 
 function App_m()
 {
-  
+    const [list,setList] = useState(news_data_m);
     const renderNews2 = ({item})=><NewsCard2 products={item}/>;
     const isTrue = ({items})=> <NewsCard2 products={items}/>;
+
+    const handleSearch = text => {
+        const filteredList = news_data_m.filter(song => {
+            const searchedText= text.toLowerCase();
+            const currentTitle= song.title.toLowerCase();
+
+            return currentTitle.indexOf(searchedText) > -1;
+        });
+        setList(filteredList);
+    }
+
     return (
         
         <SafeAreaView style={styles.container}>
     
 
          <Text style={styles.headerText}>PATIKASTORE</Text>
+         <SearchBar onSearch={handleSearch}/>
+
          <SafeAreaView style={styles.inner}>
          <FlatList horizontal={false} 
          numColumns={2}
             keyExtractor={item=>item.u_id.toString}
-             data={news_data_m}
+             data={list}
              renderItem={renderNews2}
              />
         
